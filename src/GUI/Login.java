ea995a2a5a99d9611e3e0555153bdca40a86a287
ViethungNg <ngvhungcv@gmail.com>
2024-06-11 23:57:02 +0700

    Revert "Them1 ti"
    This reverts commit b6c10eb97149ec032975f2c41a1760c8eb27aab8.

--- src/GUI/Login.java
+++ src/GUI/Login.java
@@ -21,7 +21,7 @@ public class Login extends javax.swing.JFrame {
     public Login() {
         initComponents();
     }
-//OK
+
     /**
      * This method is called from within the constructor to initialize the form.
      * WARNING: Do NOT modify this code. The content of this method is always
