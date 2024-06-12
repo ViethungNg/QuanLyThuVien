f93db0ebe7b40bb0b32eaca78253388916a04eb0
ViethungNg <ngvhungcv@gmail.com>
2024-06-12 09:36:52 +0700

    test

--- src/GUI/haha.java
+++ src/GUI/haha.java
@@ -16,7 +16,7 @@ public class haha extends javax.swing.JFrame {
     public haha() {
         initComponents();
     }
-
+//haha
     /**
      * This method is called from within the constructor to initialize the form.
      * WARNING: Do NOT modify this code. The content of this method is always
